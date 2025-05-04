931e0008f4c8a8b022954f35f0ff0ebcc267559a
Ivan Tolmachev <ivatolm@gmail.com>
2025-05-04 22:18:53 +0000

    Add SCANF_CONFIG_TraceMessage

--- src/messages.h
+++ src/messages.h
@@ -6,11 +6,24 @@
 #define SCANF_MAX_TASK_NAME_LEN 64
 
 typedef enum __attribute__((__packed__)) {
-    SCANF_TASK_CREATE = 0,
+    SCANF_CONFIG = 0,
+    SCANF_TASK_CREATE,
     SCANF_TASK_SWITCHED_IN,
     SCANF_TASK_SWITCHED_OUT
 } SCANF_EventType;
 static_assert(sizeof(SCANF_EventType) == 1, "sizeof(SCANF_EventType) > 1");
+
+typedef struct __attribute__((__packed__)) {
+    uint8_t major;
+    uint8_t minor;
+    uint8_t patch;
+} SCANF_Version;
+
+typedef struct __attribute__((__packed__)) {
+    SCANF_EventType event_type;
+    SCANF_Version version;
+    uint8_t config_max_task_name_len;
+} SCANF_CONFIG_TraceMessage;
 
 typedef struct __attribute__((__packed__)) {
     SCANF_EventType event_type;
--- src/scanf.c
+++ src/scanf.c
@@ -1,46 +1,43 @@
 #include "scanf.h"
 
 #include <FreeRTOS.h>
-#include <task.h>
-
-#include <stddef.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdio.h>
 #include <errno.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <task.h>
 
 static SCANF_Tracelog *_tracelog = NULL;
 static bool _is_tracing = false;
 
-void scanf_init(SCANF_Tracelog *tracelog)
-{
+void scanf_init(SCANF_Tracelog *tracelog) {
     if (!tracelog) return;
     _tracelog = tracelog;
+    scanf_reset();
 }
 
-SCANF_Tracelog *scanf_get_tracelog()
-{
-    return _tracelog;
-}
+SCANF_Tracelog *scanf_get_tracelog() { return _tracelog; }
 
-void scanf_reset()
-{
+void scanf_reset() {
     if (!_tracelog) return;
     _tracelog->size = 0;
+
+    static SCANF_CONFIG_TraceMessage config_message;
+    config_message.event_type = SCANF_CONFIG;
+    // TODO: Pass version through CMake
+    config_message.version.major = 0;
+    config_message.version.minor = 2;
+    config_message.version.patch = 0;
+    config_message.config_max_task_name_len = SCANF_MAX_TASK_NAME_LEN;
+    _scanf_save_trace((void *)&config_message, sizeof(config_message));
 }
 
-void scanf_start_tracing()
-{
-    _is_tracing = true;
-}
+void scanf_start_tracing() { _is_tracing = true; }
 
-void scanf_stop_tracing()
-{
-    _is_tracing = false;
-}
+void scanf_stop_tracing() { _is_tracing = false; }
 
-int scanf_save_tracelog(const char *filepath)
-{
+int scanf_save_tracelog(const char *filepath) {
     if (!_tracelog) return -EINVAL;
     if (_is_tracing) return -EBUSY;
     FILE *file = fopen(filepath, "ab");
@@ -56,8 +53,7 @@ int scanf_save_tracelog(const char *filepath)
     return 0;
 }
 
-void _scanf_save_trace(const void *message, uint32_t message_size)
-{
+void _scanf_save_trace(const void *message, uint32_t message_size) {
     if (!_tracelog) return;
     if (!_is_tracing) return;
     if ((_tracelog->size + message_size) >= _tracelog->capacity) return;
@@ -65,7 +61,4 @@ void _scanf_save_trace(const void *message, uint32_t message_size)
     _tracelog->size += message_size;
 }
 
-uint32_t _scanf_get_timestamp()
-{
-    return (xTaskGetTickCount() * 1000000) / configTICK_RATE_HZ;
-}
+uint32_t _scanf_get_timestamp() { return (xTaskGetTickCount() * 1000000) / configTICK_RATE_HZ; }
